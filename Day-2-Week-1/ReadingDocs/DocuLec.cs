e630bb5ac8389f0a0ecc96b2a98dc7112eec84f2
Fernand Qvyjt <miloamilion@gmail.com>
2021-04-20 11:46:09 -0400

    Red Letters and Problems with Decimals

---
+++ Day-2-Week-1/ReadingDocs/DocuLec.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ReadingDoc
+{
+    /// <summary>
+    /// Reading Documentation Lecture code
+    /// </summary>
+    class MainClass
+    {
+        /// <summary>
+        /// Demonstrates reading documentation
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        public static void Main(string[] args)
+        {
+            // calculate and print cosine of 45 degrees
+            float cosine = 0.8;
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Day-2-Week-1/ReadingDocs/notes.md
@@ -0,0 +1,8 @@
+# ***Reading Documentation***
+Tips on how to practice reading documentation for any programming language issue you may have.
+    - Follow along with [this](DocuLec.cs) lecture code!
+
+1. Whenever you assign a decimal number to a floating point number, the compiler will automatically assumed it's a double. 
+    - ie. : `float cosine = 0.8;` error given → <span style="color:red"> "Literal of type double cannot be implicitly coverted to type 'float', use an 'F' suffic to create a literal of this type."</span>
+
+2. 
