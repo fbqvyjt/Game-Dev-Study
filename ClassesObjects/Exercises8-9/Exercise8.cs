4c46723a507f2b219b9472d8bbfd48fc4e5827ff
Fernand Qvyjt <miloamilion@gmail.com>
2021-04-22 11:21:40 -0400

    Review of Chapter and Completed Exercise 8

---
+++ ClassesObjects/Exercises8-9/Exercise8.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    /// <summary>
+    /// Exercise 8 solution
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demonstrates rolling and using two dice
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            // initialize random number generator
+            RandomNumberGenerator.Initialize();
+
+            // create two dice
+            Die die1 = new Die();
+            Die die2 = new Die();
+
+            // tell the dice to roll themselves
+            die1.Roll();
+            die2.Roll();
+
+            // print the top sides and the sum of the dice
+            Console.WriteLine("Die 1 rolled a: " + die1.TopSide);
+            Console.WriteLine("Die 2 rolled a: " + die2.Topside);
+           
+            int sum = die1.TopSide + die2.TopSide;
+            Console.WriteLine("The sum of the dice is: " + sum);
+
+            Console.WriteLine();
+        }
+    }
+}
