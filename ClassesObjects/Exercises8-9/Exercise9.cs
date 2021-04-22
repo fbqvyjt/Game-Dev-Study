8456adbd2b949c11d13e532a37a1c12d512b6c1a
Fernand Qvyjt <miloamilion@gmail.com>
2021-04-22 11:49:43 -0400

    Finished Exercise 9

---
+++ ClassesObjects/Exercises8-9/Exercise9.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{    
+    /// <summary>
+    /// Exercise 9 solution 
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demonstrates using custom classes
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            // create a new deck and tell the deck to print itself
+            Deck deck = new Deck();
+            deck.Print();
+
+            // tell the deck to shuffle and print itself
+            deck.Shuffle();
+            deck.Print();
+            Console.WriteLine();
+
+            // take the top card from the deck and print the card rank and suit
+            Card card = deck.TakeTopCard();
+            Console.WriteLine();
+            Console.WriteLine(card.Rank + " of " + card.Suit);
+
+            Console.WriteLine();
+        }
+    }
+}
